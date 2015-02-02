7a677180f0d90472a9d564b806e2413adeeecce9
Justin Jiang <justinjiang.rws@berkeley.edu>
2015-02-02 14:49:43 -0800

    hw1 done

--- assignments/hw1/hw1.R
+++ assignments/hw1/hw1.R
@@ -40,24 +40,28 @@
 # Create the variable
 # << y >> : a vector of length 100 which has the even numbers from 2 to 200.
 # For clarity put your code here, directly below the 
-
+y <- 2*c(1:100)
 
 # Create the variable
 # << z >> : a vector of length 20 with character entries, "hw1", "hw2", ..., "hw20"
+z <- c("hw1", "hw2", "hw3", "hw4", "hw5", "hw6", "hw7", "hw8", "hw9", "hw10", "hw11", "hw12", "hw13", "hw14", "hw15", "hw16", "hw17", "hw18", "hw19", "hw20")
 
 # Create the variable
 # << m >> : a vector of length 100 with entries from a standard normal distribution
 set.seed(42)
+m <- rnorm(100)
 
 # Create the variable
 # << mean.m >> : a scalar, the mean of the numbers in << m >>
+mean.m <- mean(m)
 
 # Creat the variable
 # << sd.m >> : a scalar, the standard deviation of the numbers in << m >>
+sd.m <- sd(m)
 
 # Create the variable
 # << max.m >> : a scalar, the maximum of the numbers in << m >>
-
+max.m <- max(m)
 
 ##################################################################################
 ### Second part, data frames
@@ -78,25 +82,26 @@ load("family.rda")
 
 # Create a new data frame 
 # << family.men >> : a data frame that is a subset of family, with only the men
-
+family.men <- family[family$gender == 'm',]
 
 # Create a new data frame 
 # << family.young >> : a data frame, subset of family, with only people *under* 40
-
+family.young <- family[family$age < 40,]
 
 # Create a new data frame 
 # << family.30y68i >> : a data frame, subset of family, with only people *over* 30, *shorter* than 68 in
-
-
+family.30y <- family[family$age > 30,]
+family.30y68i <- family.30y[family$height < 68,]
 # Formula for BMI : BMI = (weight in lbs) / (height in in)^2 * 703
 # Note: the dataframe has weight in lbs and height in in as required.
 # Create a new variable 
 # << bmi >> : a vector with the BMI of each family member 
+bmi <- family$weight / (family$height)^2 *703
 
 
 # Create a new data frame
 # << family2 >> : family with an added column of BMI, with column name bmi
-
+family2 <- data.frame(family$name, family$gender, family$age, family$height, family$weight, bmi)
 
 
 ##################################################################################
