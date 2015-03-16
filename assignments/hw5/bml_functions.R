7bb4d2c9d3d60d8c2c8aedaae18c860cedefa3e3
Justin Jiang <justinjiang.rws@berkeley.edu>
2015-03-15 19:11:05 -0700

    hw5 done

--- assignments/hw5/bml_functions.R
+++ assignments/hw5/bml_functions.R
@@ -8,9 +8,12 @@
 ## that stores the state of the system (i.e. location of red and blue cars)
 
 bml.init <- function(r, c, p){
-
-  
-   #return(m)
+  if (p > 1 | p < 0){
+    stop ("Invalid Probability (p must be between 0 and 1.)")
+  }
+  samp <- sample(0:2, size = r*c, replace = TRUE, prob = c(1-p,p/2,p/2))
+  m <- matrix(data = samp,nrow = r, ncol = c)
+  return(m)
 }
 
 #### Function to move the system one step (east and north)
@@ -22,15 +25,87 @@ bml.init <- function(r, c, p){
 ## you can write extra functions that do just a step north or just a step east.
 
 bml.step <- function(m){
-
-  
-   #return(list(m, grid.new))
+  m_new <- m
+  grid.new <- TRUE
+  for (i in 1:nrow(m)){ #forbasecase
+#     can_move_changes <- NULL
+    for (j in 1:ncol(m)){
+#      print (paste0("column",j))
+      can_move <- FALSE
+      if (m[i,j] == 1){
+        if (j == ncol(m)){
+          if (m[i,1] == 0){
+            m_new[i,j] <- 0
+            m_new[i,1] <- 1
+#            print (m_new)
+          }
+        }else{
+          if (m[i,j+1] == 0){
+            m_new[i,j] <- 0
+            m_new[i,j+1] <- 1
+#            print (m_new)
+          }
+        }  
+      }
+#       print(can_move)
+#       can_move_changes <- c(can_move_changes,can_move)
+#       print(can_move_changes)
+    }
+#     for (k in 1:length(can_move_changes)){
+#       if (can_move_changes[k] == TRUE){
+#         m[i,j] <- 0
+#         if (j == ncol(m)){
+#           m[i,1] <- 1
+#         }else{
+#           m[i,j + 1] <- 1
+#         }
+#       } 
+#     }
+  }
+  m_new2 <- m_new
+  for (i in 1:nrow(m)){
+    for (j in 1:ncol(m)){
+#       can_move <- FALSE
+      if (m[i,j] == 2){
+        if (i == 1){
+          if (m_new[nrow(m),j] == 0){
+            m_new2[i,j] <- 0
+            m_new2[nrow(m),j] <- 2
+          }
+        }else{
+          if (m_new[i-1,j] == 0){
+            m_new2[i,j] <- 0
+            m_new2[i-1,j] <- 2
+          }
+        }  
+      }
+    }
+  }
+  if (all(m == m_new2)){
+    grid.new <- FALSE
+  }
+  return(list(m_new2, grid.new))
 }
 
 #### Function to do a simulation for a given set of input parameters
 ## Input : size of grid [r and c] and density [p]
 ## Output : *up to you* (e.g. number of steps taken, did you hit gridlock, ...)
 
-bml.sim <- function(r, c, p){
-
+bml.sim <- function(r, c, p, step = 1000){
+  mat <- bml.init(r,c,p)
+  gridlock <- FALSE
+  print (mat)
+  m1 <- bml.step(mat)
+  steps_taken <- 1
+  print (m1)
+  while (gridlock == FALSE & steps_taken < step){
+    if (m1[[2]] == FALSE){
+      gridlock <- TRUE
+    }
+    m1 <-bml.step(m1[[1]])
+    print (m1)
+    steps_taken <- steps_taken + 1
+    print (steps_taken)
+  }
+ return (steps_taken)
 }
--- assignments/hw5/bml_simulation.R
+++ assignments/hw5/bml_simulation.R
@@ -7,4 +7,54 @@
 #### The output can e.g. be how many steps the system took until it hit gridlock or
 #### how many steps you observered before concluding that it is in a free flowing state.
 
+par(las = 2)
 
+bml.run <- function(r, c, p, step = 5000, runsize){
+  list <- c()
+  counter <- 1
+  while (counter < runsize + 1){
+    m <- bml.sim(r,c,p, step)
+    counter <- counter + 1
+    list <- c(list, m)
+  }
+  return (list)
+}
+
+
+p_val.study <- function (r, c, step = 5000, runsize){
+  count <- 0.1
+  list <- c()
+  while (count < 1){
+    run <- bml.run(r,c,count,step,runsize)
+    list <- c(list,run)
+    count <- count + 0.1
+  }
+  study <- matrix(list,nrow = runsize, dimnames = list(c(paste0("run",1:runsize)), c(paste0("pval",seq(.1,1,by=0.1)))))
+  return (study)
+}
+
+pval_mat <- p_val.study(5,5,step = 5000,50)
+#bplot_pval <- boxplot(study_mat, main = "Steps Until Gridlock Against P-Value", xlab = "P-value",ylab = "Steps Until Gridlock")
+
+runsize.study <- function (r, c, p, step = 5000, start, end){
+  count <- start
+  list <- c()
+  while (count < end){
+    run <- bml.run(r,c,p,step,count)
+    list <- c(list,run)
+    for (i in count:(end - 1)){
+        list <- c(list,NA)
+    }
+    count <- count + 1
+  }
+  study <- matrix(list,nrow = end)
+  return (study)
+}
+
+#runsize_mat <- runsize.study(15,15, 0.5, step = 5000,25,50)
+#bplot_runsize <- boxplot(runsize_mat, main = "Steps Until Gridlock Against Runsize",xlab = "Runsize",ylab = "Steps Until Gridlock")
+
+#p_val.study(r,c,step = 5000,50)
+#runsize.study(r,c,0.5,step = 5000, 25, 50)
+
+#do this for different values of r,c
